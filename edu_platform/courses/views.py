4399b8be88ee7a31fb525bda7ab49f74422ecf38
Dmitriy Shtefan <dmitriy.shtefan@gameloft.com>
2026-04-23 20:58:06 +0300

    Current Progress

--- edu_platform/courses/views.py
+++ edu_platform/courses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
 
-# Create your views here.
+# home view
+def home(request):
+    http_method = request.method
+    return HttpResponse(f"""<h1>Home Page!</h1>
+                            <h3>HTTP Method: {http_method}</h3>
+                        """)
+
+
+# about view
+def about(request):
+    pass
+
+def courses(request):
+    pass
