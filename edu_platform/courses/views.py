ac6e8d471819077452d59f3022e886c20151a4bc
Dmitriy Shtefan <dmitriy.shtefan@gameloft.com>
2026-04-28 20:44:36 +0300

    Add modules list

--- edu_platform/courses/views.py
+++ edu_platform/courses/views.py
@@ -54,3 +54,18 @@ def get_courses_list() -> list:
             "description": "Створення серверної логіки веб-додатків на Python (Django), робота з базами даних та обробка HTTP-запитів."
         },
     ]
+
+def get_modules_list() -> list:
+    return [
+        {"name": "Django: Setup",            "number_of_classes": 2},
+        {"name": "Django: Templates",        "number_of_classes": 4},
+        {"name": "Django: Models",           "number_of_classes": 6},
+        {"name": "Django: Views",            "number_of_classes": 5},
+        {"name": "Django: URLs",             "number_of_classes": 3},
+        {"name": "Django: Forms",            "number_of_classes": 4},
+        {"name": "Django: Admin",            "number_of_classes": 2},
+        {"name": "Django: Authentication",   "number_of_classes": 5},
+        {"name": "Django: ORM Advanced",     "number_of_classes": 6},
+        {"name": "Django: REST API",         "number_of_classes": 7},
+        {"name": "Django: Testing",          "number_of_classes": 3},
+    ]
