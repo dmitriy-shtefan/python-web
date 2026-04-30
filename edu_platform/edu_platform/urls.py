899e49b983b0527071237b6318ac55e345bde5a8
Dmytro <shtefan.dmitry@gmail.com>
2026-04-30 19:57:05 +0300

    Current Progress

--- edu_platform/edu_platform/urls.py
+++ edu_platform/edu_platform/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from courses import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("courses/", include("courses.urls")),
+    path("", views.index)
 ]
